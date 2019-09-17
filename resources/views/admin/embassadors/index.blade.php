e216a0686ccceaabeca1134eb3872886d5bef65e
hanyaboutaleb <henooo2012@gmail.com>
2019-09-17 15:35:07 +0200

    a

--- resources/views/admin/embassadors/index.blade.php
+++ resources/views/admin/embassadors/index.blade.php
@@ -1,18 +1,13 @@
 @extends('admin.master.app')
-
 @section('content')
-
-
-
 <div class="kt-portlet kt-portlet--mobile">
-
   <div class="kt-portlet__head kt-portlet__head--lg">
     <div class="kt-portlet__head-label">
       <span class="kt-portlet__head-icon">
         <i class="kt-font-brand flaticon2-line-chart"></i>
       </span>
       <h3 class="kt-portlet__head-title">
-بيانات السفراء      </h3>
+        بيانات السفراء </h3>
     </div>
 
     <div class="kt-portlet__head-toolbar">
@@ -23,12 +18,12 @@
             اضافة سفير
           </a>
         </div>
-
       </div>
     </div>
-
   </div>
   <div class="kt-portlet__body">
+
+  
     <!--begin: Datatable -->
     <table class="table table-striped- table-bordered table-hover table-checkable" id="kt_table_1">
       <thead>
@@ -40,9 +35,7 @@
           <th>رقم التليفون</th>
           <th> تاريخ الميلاد</th>
           <th> اسم الشريك</th>
-
           <th colspan="2">العمليات</th>
-
         </tr>
       </thead>
       <tbody id='table-result'>
@@ -55,8 +48,6 @@
           <td>{{$embassador->phone}}</td>
           <td>{{$embassador->birth_date}}</td>
           <td>{{$embassador->agent_name}}</td>
-
-
           <td>
             <a href="{{ route('embassador.edit', $embassador->embassador_id)}}" class="btn btn-primary">تعديل</a>
           </td>
@@ -75,18 +66,13 @@
     </table>
     <!--end: Datatable -->
   </div>
-
-
-
   <div class="row">
     <div class="col-sm-12 col-md-5">
       <div class="col-sm-12 col-md-7 dataTables_pager">
         <div class="dataTables_length" id="kt_table_1_length">
-        {{ $all_embassdors_cities->onEachSide(50)->links() }}
-
+          {{ $all_embassdors_cities->onEachSide(50)->links() }}
         </div>
         <div class="dataTables_paginate paging_simple_numbers" id="kt_table_1_paginate">
-
         </div>
       </div>
     </div>
