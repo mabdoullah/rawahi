6b6940c3dfc54713b3efd641725605b2d703d7ea
Ahmed El-Sedemy <ahmed_sedamy@yahoo.com>
2019-09-22 18:44:01 +0200

    update

--- resources/views/admin/embassadors/index.blade.php
+++ resources/views/admin/embassadors/index.blade.php
@@ -36,7 +36,7 @@
     <div class="kt-portlet__head-toolbar">
       <div class="kt-portlet__head-wrapper">
         <div class="kt-portlet__head-actions">
-          <a href="/admin/embassador/create" class="btn btn-brand btn-elevate btn-icon-sm">
+          <a href="{{url('admin/embassador/create')}}" class="btn btn-brand btn-elevate btn-icon-sm">
             <i class="la la-plus"></i>
             اضافة سفير
           </a>
